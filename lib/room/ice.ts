25fdccb496f7c90a2d9a7b15c66f071e5197708a
LHM52 <dlgudals8410@naver.com>
2026-05-05 23:17:26 +0900

    turn 서버 추가

--- lib/room/ice.ts
+++ lib/room/ice.ts
@@ -1,18 +1,10 @@
-const turnUrl = process.env.NEXT_PUBLIC_TURN_URL;
-const turnUsername = process.env.NEXT_PUBLIC_TURN_USERNAME;
-const turnCredential = process.env.NEXT_PUBLIC_TURN_CREDENTIAL;
-
 export const defaultRtcConfiguration: RTCConfiguration = {
   iceServers: [
-    { urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"] },
-    ...(turnUrl && turnUsername && turnCredential
-      ? [
-          {
-            urls: turnUrl,
-            username: turnUsername,
-            credential: turnCredential,
-          },
-        ]
-      : []),
+    { urls: ["stun:stun.l.google.com:19302"] },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
   ],
 };
